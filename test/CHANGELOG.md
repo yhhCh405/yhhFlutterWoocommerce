5411b988485372c9528c1bcb13e6fbff2d4d5369
yehtethein <yehtethein59@gmail.com>
2020-06-05 00:43:22 +0630

    init

--- test/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## [0.0.1-snowflake.0] - 2020-06-4
-
-* Initial Release.
--- test/android/app/src/main/kotlin/com/yhh/flutter_woocommerce/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.yhh.flutter_woocommerce
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
