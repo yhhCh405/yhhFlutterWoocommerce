1ca7145984c294b2a082e8bcf8ae14818786607e
yehtethein <yehtethein59@gmail.com>
2020-06-11 09:50:53 +0630

    0.0.2 stable

--- example/lib/config.dart
+++ example/lib/config.dart
@@ -1,9 +1,5 @@
 class ServerCrends {
-  static String apiUrl = "https://shop1.techplussolution.com";
-  static String consumerKey = 'ck_73ea17d800c5bd9037253caee7c2026439e2bf74';
-  static String consumerSecret = 'cs_fab2fbf3bea122e2d087bb224a0c49cd0e9b23a9';
-
-  // static String apiUrl = "<your site address>";
-  // static String consumerKey = '<consumer key>';
-  // static String consumerSecret = '<consumer secret>';
+  static String apiUrl = "<your site address>";
+  static String consumerKey = '<consumer key>';
+  static String consumerSecret = '<consumer secret>';
 }
